794951ceaa8ee1634c04cb90dcaf5a5fa2d928a3
Viollo <vadim.siraev@bk.ru>
2022-05-25 19:55:27 +0300

    lab 2

---
+++ script.bat
@@ -0,0 +1,20 @@
+echo off
+md Siraev
+cd Siraev
+md Vadim
+md Vyacheslavovich
+pause
+echo > 05062001.txt
+cd Vyacheslavovich
+echo > computer_011.txt
+pause
+cd ..
+cd ..
+del Siraev /S /Q /F
+pause
+cd Siraev
+rd Vyacheslavovich
+rd Vadim
+cd ..
+rd Siraev
+pause
